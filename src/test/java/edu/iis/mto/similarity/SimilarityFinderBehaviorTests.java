75a9723eb2343add34098319c64418b713416a60
Tobiasz Kubiak <tobiaszkubiak31@gmail.com>
2020-04-01 19:06:51 +0200

    Implement Behavior Test FirstSequenceIsNull of SimilarityFinder class

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviorTests.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviorTests.java
@@ -1,13 +1,35 @@
 package edu.iis.mto.similarity;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import edu.iis.mto.search.SequenceSearcherStyleDoubler;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SimilarityFinderBehaviorTests {
 
-  @Test
-  void FirstSequenceIsNull() {
+  private SequenceSearcherStyleDoubler sequenceSearcherStyleDoubler;
+  private SimilarityFinder similarityFinder;
+  private int[] seq1;
+  private int[] seq2;
+
+  @BeforeEach
+  void setUpVariables() {
+    sequenceSearcherStyleDoubler = new SequenceSearcherStyleDoubler();
+    similarityFinder = new SimilarityFinder(sequenceSearcherStyleDoubler);
 
   }
+
+  @Test
+  void FirstSequenceIsNull() {
+    seq1 = null;
+    seq2 = new int[]{1, 5, 4};
+
+    assertThrows(
+        NullPointerException.class,
+        () -> this.similarityFinder.calculateJackardSimilarity(seq1, seq2));
+  }
+
   @Test
   void SecondSequenceIsNull() {
 
@@ -24,7 +46,7 @@ public class SimilarityFinderBehaviorTests {
   }
 
   @Test
-  void checkAmountOfOperatesSequenceSearcher(){
+  void checkAmountOfOperatesSequenceSearcher() {
 
   }
 
